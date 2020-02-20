d49e38ddd18fb65028334234bf964489192e2991
usama@inexisconsulting.com <Jumaloon12@#>
2020-02-20 13:45:14 +0530

    Login page and routing

---
+++ Help247/ClientApp/src/app/admin/auth/login/login.component.ts
@@ -0,0 +1,42 @@
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss']
+})
+export class LoginComponent implements OnInit {
+
+  loginForm: FormGroup;
+  hide = true;
+
+  constructor(
+    private fb: FormBuilder,
+    private router: Router
+  ) { }
+
+  ngOnInit() {
+    this.initForm();
+  }
+
+  initForm() {
+    this.loginForm = this.fb.group({
+      username: ['', [Validators.required]],
+      password: ['', [Validators.required]]
+    })
+  }
+
+  get username() {
+    return this.loginForm.get('username');
+  }
+  get password() {
+    return this.loginForm.get('password');
+  }
+
+  dashboard(){
+    this.router.navigate(['/dashboard']);
+  }
+
+}
--- Help247/ClientApp/src/app/app.module.ts
+++ Help247/ClientApp/src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import { AdminLayoutComponent } from './admin/layouts/admin-layout/admin-layout.component';
 import { ComponentsModule } from './admin/components/components.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -16,7 +17,9 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     BrowserModule,
     AppRoutingModule,
     ComponentsModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
